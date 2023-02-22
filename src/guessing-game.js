860c46398de573caf22c7900f36a4f51de5124f5
millisabel <elena.kozlova.job@gmail.com>
2023-02-22 18:53:18 +0100

    feat: decided

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.middle = null;
+    }
 
     setRange(min, max) {
+        this.min = min;
+        this.max  = max;
 
+        this.guess();
     }
 
     guess() {
-
+        if(this.min < this.max){
+            this.middle = Math.round((this.min + this.max) / 2);
+        }
+        return this.middle;
     }
 
     lower() {
-
+        this.max  = this.middle;
     }
 
     greater() {
-
+        this.min  = this.middle;
     }
+
+
 }
 
 module.exports = GuessingGame;
